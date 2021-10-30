056d6584a097d46b8734c24548d1c5b8f792327b
Aamir Farooq <aamir@farooq.xyz>
2021-10-30 23:20:04 +0200

    Reworked converters

--- utils/checks.py
+++ utils/checks.py
@@ -4,7 +4,6 @@ from discord.member import Member
 from data.services.guild_service import guild_service
 from utils.context import BlooContext
 
-from utils.database import db
 from utils.permissions import permissions
 
 
@@ -34,50 +33,6 @@ def whisper():
         return True
     return commands.check(predicate)
 
-
-class ModsAndAboveMember(commands.Converter):
-    async def convert(self,  ctx: BlooContext, argument):
-        user = await commands.MemberConverter().convert(ctx, argument)
-        await check_invokee(ctx, user)
-        return user
-
-class ModsAndAboveExternal(commands.Converter):
-    async def convert(self,  ctx: BlooContext, argument):
-        try:
-            user = await commands.MemberConverter().convert(ctx, argument)
-        except PermissionsFailure as e:
-            raise e   
-        except Exception:
-            try:
-                argument = int(argument)
-                user = await ctx.bot.fetch_user(argument)
-            except Exception:
-                raise PermissionsFailure("Could not parse argument \"user\".")
-            except NotFound:
-                raise PermissionsFailure(
-                    f"Couldn't find user with ID {argument}")
-            
-        await check_invokee(ctx, user)
-        return user 
-
-async def check_invokee(ctx, user):
-    if isinstance(user, Member):
-        if user.id == ctx.author.id:
-            await ctx.message.add_reaction("🤔")
-            raise PermissionsFailure("You can't call that on yourself.")
-        
-        if user.id == ctx.bot.user.id:
-            await ctx.message.add_reaction("🤔")
-            raise PermissionsFailure("You can't call that on me :(")
-        
-        if user:
-                if isinstance(user, Member):
-                    if user.top_role >= ctx.author.top_role:
-                        raise PermissionsFailure(
-                            message=f"{user.mention}'s top role is the same or higher than yours!")
-
-
-### 
 
 def memplus_and_up():
     async def predicate(ctx: BlooContext):
---
+++ utils/converters.py
@@ -0,0 +1,61 @@
+from discord.errors import NotFound
+from discord.ext import commands
+from discord.member import Member
+from utils.checks import PermissionsFailure
+from utils.context import BlooContext
+
+
+async def mods_and_above_member_resolver(ctx: BlooContext, argument):
+    user = await commands.MemberConverter().convert(ctx, argument)
+    await check_invokee(ctx, user)
+    return user
+
+
+async def mods_and_above_external_resolver(ctx: BlooContext, argument):
+    try:
+        user = await commands.MemberConverter().convert(ctx, argument)
+    except PermissionsFailure as e:
+        raise e   
+    except Exception:
+        try:
+            argument = int(argument)
+            user = await ctx.bot.fetch_user(argument)
+        except Exception:
+            raise PermissionsFailure("Could not parse argument \"user\".")
+        except NotFound:
+            raise PermissionsFailure(
+                f"Couldn't find user with ID {argument}")
+        
+    await check_invokee(ctx, user)
+    return user 
+
+
+async def user_resolver(ctx: BlooContext, argument):
+    try:
+        argument = int(argument)
+        user = await ctx.bot.fetch_user(argument)
+    except Exception:
+        raise PermissionsFailure("Could not parse argument \"user\".")
+    except NotFound:
+        raise PermissionsFailure(
+            f"Couldn't find user with ID {argument}")
+        
+    return user 
+
+
+async def check_invokee(ctx, user):
+    if isinstance(user, Member):
+        if user.id == ctx.author.id:
+            await ctx.message.add_reaction("🤔")
+            raise PermissionsFailure("You can't call that on yourself.")
+        
+        if user.id == ctx.bot.user.id:
+            await ctx.message.add_reaction("🤔")
+            raise PermissionsFailure("You can't call that on me :(")
+        
+        if user:
+                if isinstance(user, Member):
+                    if user.top_role >= ctx.author.top_role:
+                        raise PermissionsFailure(
+                            message=f"{user.mention}'s top role is the same or higher than yours!")
+

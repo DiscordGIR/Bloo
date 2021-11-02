5a9bc1c909f6a6941cc1bdcc2124ae40769ea2cb
ja1dan <ajaidan0@gmail.com>
2021-11-02 14:41:59 -0400

    Code, message and kill cleanup

--- monitor.py
+++ monitor.py
@@ -1,40 +1,53 @@
-import subprocess
-import os
-import platform
+import subprocess, sys, platform, psutil
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+if '--no-color' in sys.argv:
+    class bcolors:
+        HEADER = ''
+        OKBLUE = ''
+        OKCYAN = ''
+        OKGREEN = ''
+        WARNING = ''
+        FAIL = ''
+        ENDC = ''
+        BOLD = ''
+        UNDERLINE = ''
+else:
+    class bcolors:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKCYAN = '\033[96m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
 
 proc = "N/A"
+
+def kill(proc_pid):
+    process = psutil.Process(proc_pid)
+    for proc in process.children(recursive=True):
+        proc.kill()
+    process.kill()
 
 if platform.system() == "Windows":
     proc = subprocess.Popen(['python', './main.py'], shell=True)
 else:
     proc = subprocess.Popen('python ./main.py', shell=True)
-    
+
 def startProc():
     if platform.system() == "Windows":
         proc = subprocess.Popen(['python', './main.py'], shell=True)
     else:
         proc = subprocess.Popen('python ./main.py', shell=True)
-
-def termProc():
-    proc.terminate()
     
 def restartProc():
-    print(f"{bcolors.FAIL}[!] Terminating old process...{bcolors.ENDC}")
-    termProc()
-    print(f"{bcolors.OKGREEN}[^] Starting new process...{bcolors.ENDC}")
+    print(f"{bcolors.FAIL}[monitor] Terminating old process...{bcolors.ENDC}")
+    kill(proc.pid)
+    print(f"{bcolors.OKGREEN}[monitor] Starting new process...{bcolors.ENDC}")
     startProc()
 
 class EventHandler(FileSystemEventHandler):
@@ -46,32 +59,32 @@ class EventHandler(FileSystemEventHandler):
         if event.src_path == '.':
             return
         if event.src_path.endswith('.py') | event.src_path.endswith('.json'):
-            print(f"{bcolors.OKGREEN}[*] File creation detected ({bcolors.ENDC}{bcolors.UNDERLINE}{event.src_path}{bcolors.ENDC}{bcolors.OKGREEN})\n[^] Restarting...{bcolors.ENDC}")
+            print(f"{bcolors.OKGREEN}[monitor] File creation detected ({bcolors.ENDC}{bcolors.UNDERLINE}{event.src_path}{bcolors.ENDC}{bcolors.OKGREEN})\n[^] Restarting...{bcolors.ENDC}")
             restartProc()
 
     def on_deleted(self, event):
         if event.src_path == '.':
             return
         if event.src_path.endswith('.py') | event.src_path.endswith('.json'):
-            print(f"{bcolors.OKGREEN}[*] File deletion detected ({bcolors.ENDC}{bcolors.UNDERLINE}{event.src_path}{bcolors.ENDC}{bcolors.OKGREEN})\n[^] Restarting...{bcolors.ENDC}")
+            print(f"{bcolors.OKGREEN}[monitor] File deletion detected ({bcolors.ENDC}{bcolors.UNDERLINE}{event.src_path}{bcolors.ENDC}{bcolors.OKGREEN})\n[^] Restarting...{bcolors.ENDC}")
             restartProc()
 
     def on_modified(self, event):
         if event.src_path == '.':
             return
         if event.src_path.endswith('.py') | event.src_path.endswith('.json'):
-            print(f"{bcolors.OKGREEN}[*] File change detected ({bcolors.ENDC}{bcolors.UNDERLINE}{event.src_path}{bcolors.ENDC}{bcolors.OKGREEN})\n[^] Restarting...{bcolors.ENDC}")
+            print(f"{bcolors.OKGREEN}[monitor] File change detected ({bcolors.ENDC}{bcolors.UNDERLINE}{event.src_path}{bcolors.ENDC}{bcolors.OKGREEN})\n[^] Restarting...{bcolors.ENDC}")
             restartProc()
 
     def on_moved(self, event):
         if event.src_path == '.':
             return
         if event.src_path.endswith('.py') | event.src_path.endswith('.json'):
-            print(f"{bcolors.OKGREEN}[*] File move detected ({bcolors.ENDC}{bcolors.UNDERLINE}{event.src_path}{bcolors.ENDC}{bcolors.OKGREEN})\n[^] Restarting...{bcolors.ENDC}")
+            print(f"{bcolors.OKGREEN}[monitor] File move detected ({bcolors.ENDC}{bcolors.UNDERLINE}{event.src_path}{bcolors.ENDC}{bcolors.OKGREEN})\n[^] Restarting...{bcolors.ENDC}")
             restartProc()
 
 if __name__ == "__main__":
-    print(f"{bcolors.OKGREEN}[^] Starting process...{bcolors.ENDC}")
+    print(f"{bcolors.OKGREEN}[monitor] Starting process...{bcolors.ENDC}")
     event_handler = EventHandler()
     observer = Observer()
     observer.schedule(event_handler, '.', recursive=True)
@@ -80,8 +93,8 @@ if __name__ == "__main__":
         while True:
             pass
     except KeyboardInterrupt:
-        print(f"\n{bcolors.FAIL}[!] Stopping process...{bcolors.ENDC}")
-        termProc()
+        print(f"\n{bcolors.FAIL}[monitor] Stopping process...{bcolors.ENDC}")
+        kill(proc.pid)
         observer.stop()
         exit(0)
     observer.join()

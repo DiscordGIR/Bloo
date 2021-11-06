aab36bb8e79b3daa58e8938fc0559d40796b7531
ja1dan <ajaidan0@gmail.com>
2021-11-05 21:58:21 -0400

    Comment menu code

--- utils/menu.py
+++ utils/menu.py
@@ -3,6 +3,7 @@ from utils.views.menu import MenuButtons
 
 class Menu():
     def __init__(self, pages: list, channel, format_page, interaction: bool, ctx: BlooContext, whisper: bool):
+        # Declare variables that we need to use globally throughout the menu
         self.pages = pages
         self.channel = channel
         self.page_formatter = format_page
@@ -11,5 +12,7 @@ class Menu():
         self.should_whisper = whisper
 
     async def init_menu(self):
+        # Prepare inital embed
         embed = await self.page_formatter(entry=self.pages[0], all_pages=self.pages, current_page=1)
+        # Initialize our menu
         await MenuButtons(self.ctx, self.pages, self.page_formatter, self.channel, self.is_interaction, self.should_whisper).launch(embed)
--- utils/views/menu.py
+++ utils/views/menu.py
@@ -4,7 +4,9 @@ from discord.channel import TextChannel
 
 class MenuButtons(ui.View):
     def __init__(self, ctx: BlooContext, pages: list, page_formatter, channel: TextChannel, interaction: bool, whisper: bool):
+        # Tell buttons to disable after 60 seconds
         super().__init__(timeout=60)
+        # Declare variables that we need to use globally throughout the menu
         self.channel = channel
         self.is_interaction = interaction
         self.should_whisper = whisper
@@ -18,6 +20,7 @@ class MenuButtons(ui.View):
         
     async def launch(self, embed):
         self.embed = embed
+        # See which buttons we actually need enabled
         self.previous.disabled = True
         self.next.disabled = True
         componentEnabled = False
@@ -27,9 +30,11 @@ class MenuButtons(ui.View):
         if len(self.pages) > self.current_page:
             self.next.disabled = False
             componentEnabled = True
-            
+        
+        # If we aren't in an interaction,
         if self.is_interaction is False:
             if self.msg is None:
+                # This is so we don't have buttons needlessly enabled for tweaks with only 1 page
                 if componentEnabled is True:
                     self.msg = await self.channel.send(embed=embed, view=self)
                 else:
@@ -39,7 +44,10 @@ class MenuButtons(ui.View):
                     await self.msg.edit(embed=embed, view=self)
                 else:
                     await self.msg.edit(embed=embed)
+        # Otherwise,
         else:
+            # Handle for an interaction.
+            # This is so we don't have buttons needlessly enabled for tweaks with only 1 page
             if componentEnabled is True:
                 if self.should_whisper is True:
                     await self.ctx.respond_or_edit(embed=embed, view=self, ephemeral=True)
@@ -51,28 +59,42 @@ class MenuButtons(ui.View):
                 else:
                     await self.ctx.respond_or_edit(embed=embed)
     
+    # Declare previous button
     @ui.button(emoji='⬅️', style=ButtonStyle.blurple, row=1, disabled=True)
     async def previous(self, button: ui.Button, interaction: Interaction):
         if interaction.user == self.ctx.author:
+            # Pull down array current page
             self.array_current_page = (self.array_current_page - 1)
+            # Pull down actual current page
             self.current_page = (self.current_page - 1)
+            # Prepare our embed
             embed = await self.page_formatter(entry=self.pages[self.array_current_page], all_pages=self.pages, current_page=self.current_page)
+            # Launch!
             await self.launch(embed)
             
+    # Declare stop button
     @ui.button(emoji='⏹️', style=ButtonStyle.blurple, row=1)
     async def pause(self, button: ui.Button, interaction: Interaction):
         if interaction.user == self.ctx.author:
+            # Run our timeout function early
             await self.on_timeout()
             
+    # Declare next button
     @ui.button(emoji='➡️', style=ButtonStyle.blurple, row=1, disabled=True)
     async def next(self, button: ui.Button, interaction: Interaction):
         if interaction.user == self.ctx.author:
+            # Bump up array current page
             self.array_current_page = (self.array_current_page + 1)
+            # Bump up actual current page
             self.current_page = (self.current_page + 1)
+            # Prepare our embed
             embed = await self.page_formatter(entry=self.pages[self.array_current_page], all_pages=self.pages, current_page=self.current_page)
+            # Launch!
             await self.launch(embed)
 
+    # Timeout function
     async def on_timeout(self):
+        # Check if we even have any components enabled (if we don't, we don't need to do anything!)
         componentEnabled = False
         if 0 <= (self.array_current_page - 1) < len(self.pages):
             componentEnabled = True
@@ -80,10 +102,15 @@ class MenuButtons(ui.View):
             componentEnabled = True
         if componentEnabled is False:
             return
+
+        # Recursively disable all buttons
         for child in self.children:
             child.disabled = True
+        
+        # If we aren't in an interaction, just edit the current message
         if self.is_interaction is False:
             await self.msg.edit(embed=self.embed, view=self)
+        # Otherwise, handle with context
         else:
             if self.should_whisper is True:
                 await self.ctx.respond_or_edit(embed=self.embed, view=self, ephemeral=True)

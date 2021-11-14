5005cb0eb7daa78b503298b46042f7c8bf956814
Aamir Farooq <aamir@farooq.xyz>
2021-11-14 17:21:50 +0100

    Change warn context command

--- utils/views/modactions.py
+++ utils/views/modactions.py
@@ -16,49 +16,33 @@ class WarnView(ui.View):
             return False
         return True
 
-    @ui.button(label="50", style=discord.ButtonStyle.primary)
-    async def fifty(self, button: ui.Button, interaction: discord.Interaction):
+    @ui.button(label="piracy", style=discord.ButtonStyle.primary)
+    async def piracy(self, button: ui.Button, interaction: discord.Interaction):
         if not self.check(interaction):
             return
-        reason = await self.prompt_reason(interaction)
-        if reason is None:
-            await self.ctx.send_warning("Cancelled")
-            return
 
-        await self.warn(self.ctx, self.member, 50, reason)
+        await self.warn(self.ctx, self.member, 50, "piracy")
 
-    @ui.button(label="100", style=discord.ButtonStyle.primary)
-    async def hundred(self, button: ui.Button, interaction: discord.Interaction):
+    @ui.button(label="slurs", style=discord.ButtonStyle.primary)
+    async def slurs(self, button: ui.Button, interaction: discord.Interaction):
         if not self.check(interaction):
             return
-        reason = await self.prompt_reason(interaction)
-        if reason is None:
-            await self.ctx.send_warning("Cancelled")
-            return
 
-        await self.warn(self.ctx, self.member, 100, reason)
+        await self.warn(self.ctx, self.member, 50, "slurs")
 
-    @ui.button(label="150", style=discord.ButtonStyle.primary)
-    async def hun_fifty(self, button: ui.Button, interaction: discord.Interaction):
+    @ui.button(label="filter bypass", style=discord.ButtonStyle.primary)
+    async def filter_bypass(self, button: ui.Button, interaction: discord.Interaction):
         if not self.check(interaction):
             return
-        reason = await self.prompt_reason(interaction)
-        if reason is None:
-            await self.ctx.send_warning("Cancelled")
-            return
 
-        await self.warn(self.ctx, self.member, 150, reason)
-    
-    @ui.button(label="200", style=discord.ButtonStyle.primary)
-    async def two_hun(self, button: ui.Button, interaction: discord.Interaction):
+        await self.warn(self.ctx, self.member, 50, "filter bypass")
+
+    @ui.button(label="rule 5", style=discord.ButtonStyle.primary)
+    async def rule5(self, button: ui.Button, interaction: discord.Interaction):
         if not self.check(interaction):
             return
-        reason = await self.prompt_reason(interaction)
-        if reason is None:
-            await self.ctx.send_warning("Cancelled")
-            return
 
-        await self.warn(self.ctx, self.member, 200, reason)
+        await self.warn(self.ctx, self.member, 50, "rule 5")
 
     @ui.button(emoji="❌", style=discord.ButtonStyle.primary)
     async def cancel(self, button: ui.Button, interaction: discord.Interaction):
@@ -66,14 +50,14 @@ class WarnView(ui.View):
             return
         await self.ctx.send_warning("Cancelled")
 
-    async def prompt_reason(self, interaction: discord.Interaction):
-        prompt_data = PromptData(value_name="Reason", 
-                                        description="Reason for warn?",
-                                        convertor=str,
-                                        )
-        await interaction.response.defer()
-        self.ctx.author = interaction.user
-        reason = await self.ctx.prompt(prompt_data)
-        return reason
+    # async def prompt_reason(self, interaction: discord.Interaction):
+    #     prompt_data = PromptData(value_name="Reason", 
+    #                                     description="Reason for warn?",
+    #                                     convertor=str,
+    #                                     )
+    #     await interaction.response.defer()
+    #     self.ctx.author = interaction.user
+    #     reason = await self.ctx.prompt(prompt_data)
+    #     return reason
 
     

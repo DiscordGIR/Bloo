71b59a1976bab67a8228533e97640d10c1d2c221
Aamir Farooq <aamir@farooq.xyz>
2021-11-30 12:45:07 +0500

    Some cleanup

--- GLOBALS.py
+++
@@ -1,7 +0,0 @@
-# Add arguments
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--disable-discord-logs', help='Disables Discord logging.', action='store_true')
-parser.add_argument('--disable-scheduler-logs', help='Disables scheduler logs.', action='store_true')
-args = parser.parse_args()

39ab4df3597b526d7114971b7e238fe6c95bb7b9
Aamir Farooq <aamir@farooq.xyz>
2021-12-07 23:11:44 +0500

    Better error handling, formatting

--- cogs/monitors/tweaklist.py
+++ cogs/monitors/tweaklist.py
@@ -1,6 +1,10 @@
-import discord, aiohttp, json, os
+import os
+import traceback
+
+import aiohttp
+import discord
 from discord.ext import commands
-from yarl import URL
+from utils.logger import logger
 
 
 class Tweaks(commands.Cog):
@@ -10,21 +14,36 @@ class Tweaks(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
-            pass
-        else:
-            if "apt" in message.content.lower() and "base structure" in message.content.lower() and ("libhooker" or "substitute" or "substrate" in message.content.lower()) and len(message.content.splitlines()) >= 50:
-                async with aiohttp.ClientSession(headers={'content-type': 'application/json', 'X-Auth-Token': os.environ.get("PASTEE_TOKEN")}) as session:
-                    async with session.post(url='https://api.paste.ee/v1/pastes', json={"description": f"Uploaded by {message.author}","sections":[{"name":f"Uploaded by {message.author}","syntax":"text","contents":message.content}]}) as response:
-                        resp = await response.read()
-                        pastelink = json.loads(resp).get("link")
-                        embed = discord.Embed(title=f"Tweak list", color=discord.Color.green())
-                        embed.description = f"You have pasted a tweak list, to reduce chat spam it can be viewed [here]({pastelink})."
-                        # Check if the upload was a success
-                        if pastelink is not None:
-                            await message.delete()
-                            await message.channel.send(f'{message.author.mention}', embed=embed, allowed_mentions=discord.AllowedMentions(users=True))
-                        else: pass
+            return
+
+        if not ("apt" in message.content.lower() and "base structure" in message.content.lower() and ("libhooker" or "substitute" or "substrate" in message.content.lower()) and len(message.content.splitlines()) >= 50):
+            return
+
+        async with aiohttp.ClientSession(headers={'content-type': 'application/json', 'X-Auth-Token': os.environ.get("PASTEE_TOKEN")}) as session:
+            async with session.post(url='https://api.paste.ee/v1/pastes', json={"description": f"Uploaded by {message.author}", "sections": [{"name": f"Uploaded by {message.author}", "syntax": "text", "contents": message.content}]}) as response:
+                if response.status != 201:
+                    try:
+                        raise Exception(f"Failed to upload paste: {response.status}")
+                    except Exception:
+                        logger.error(traceback.format_exc())
+
+                resp = await response.json()
+                pastelink = resp.get("link")
+                if pastelink is None:
+                    return
+
+                embed = discord.Embed(
+                    title=f"Tweak list", color=discord.Color.green())
+                embed.description = f"You have pasted a tweak list, to reduce chat spam it can be viewed [here]({pastelink})."
+
+                await message.delete()
+                await message.channel.send(message.author.mention, embed=embed)
 
 
 def setup(bot):
+    if os.environ.get("PASTEE_TOKEN") is None:
+        logger.warn(
+            "Pastee token not set, not loading the TweakList cog! If you want this, set PASTEE_TOKEN in the .env file.")
+        return
+
     bot.add_cog(Tweaks(bot))

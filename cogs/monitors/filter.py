5ad05785d3092ae1457da35d9eb17314a56e4a4e
Aamir Farooq <aamir@farooq.xyz>
2022-01-14 00:13:13 +0100

    Slight reword

--- cogs/monitors/filter.py
+++ cogs/monitors/filter.py
@@ -331,13 +331,13 @@ class Filter(commands.Cog):
 
         if any(intent in text for intent in intent_news) and subject_and_word_in_message:
             embed = discord.Embed(color=discord.Color.orange())
-            embed.description = f"It appears you are asking about future Jailbreaks. Nobody knows when a jailbreak will be released, you can get notified about releases in #announcements by going to <#{db_guild.channel_reaction_roles}>"
+            embed.description = f"It appears you are asking about future Jailbreaks. Nobody knows when a jailbreak will be released, but you can subscribe to notifications about releases by going to <#{db_guild.channel_reaction_roles}>."
             embed.set_footer(
                 text="This action was performed automatically. Please disregard if incorrect.")
             await message.reply(embed=embed)
         elif any(intent in text for intent in intent_cij) and subject_and_word_in_message:
             embed = discord.Embed(color=discord.Color.orange())
-            embed.description = "It appears you are asking if you can jailbreak your device, you can find out that information by using `/canijailbreak` or in the \"Get Started\" section of [ios.cfw.guide](https://ios.cfw.guide/get-started)"
+            embed.description = "It appears you are asking if you can jailbreak your device, you can find out that information by using `/canijailbreak` or in the \"Get Started\" section of [ios.cfw.guide](https://ios.cfw.guide/get-started)."
             embed.set_footer(
                 text="This action was performed automatically. Please disregard if incorrect.")
             await message.reply(embed=embed)

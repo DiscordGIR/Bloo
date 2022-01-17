73dbd34adeedccb2ba1178d7fab87c81c64b76d9
Aamir Farooq <aamir@farooq.xyz>
2022-01-17 18:35:34 +0100

    Improved implementation with Canister

--- cogs/monitors/repos.py
+++ cogs/monitors/repos.py
@@ -1,10 +1,8 @@
 import discord
 import re
-import aiohttp
-from urllib.request import Request, urlopen
 from discord.ext import commands
-from yarl import URL
 from data.services.guild_service import guild_service
+from utils.autocompleters import fetch_repos
 from utils.permissions.permissions import permissions
 
 
@@ -18,22 +16,28 @@ class RepoWatcher(commands.Cog):
             return
         if message.channel.id == guild_service.get_guild().channel_general and not permissions.has(message.guild, message.author, 5):
             return
-        
+
         url = re.search(r'(https?://\S+)', message.content)
         if url is None:
             return
 
-        try:
-            async with aiohttp.ClientSession() as client:
-                async with client.get(URL(f'{url.group(0)}', encoded=True)) as resp:
-                    req = Request(url.group(0)+'/Release')
-                    # some repos need this, looking at you Chariz...
-                    req.add_header('User-Agent', 'Sileo/2.5.6 CoreFoundation/1770.300 Darwin/20.2.0')
-                    if urlopen(req).code == 200:
-                        view = discord.ui.View()
-                        view.add_item(discord.ui.Button(label='Add Repo', url=f"https://sharerepo.stkc.win/?repo={url.group(0)}", emoji=":cydiasileosplit:932650041099825232", style=discord.ButtonStyle.url))
-                        await message.reply(file=discord.File("data/images/transparent1x1.png") , view=view, mention_author=False)
-        except: pass
+        repos = await fetch_repos()
+        repos = [repo['uri'].lower() for repo in repos if repo.get('uri')]
+
+        potential_repo = url.group(0).rstrip("/").lower()
+        if potential_repo not in repos:
+            return
+
+        view = discord.ui.View()
+
+        view.add_item(discord.ui.Button(label='Add Repo to Sileo', emoji="<:sileo:679466569407004684>",
+                                        url=f"https://sharerepo.stkc.win/v2/?pkgman=sileo&repo={potential_repo}", style=discord.ButtonStyle.url))
+        view.add_item(discord.ui.Button(label='Add Repo to Zebra', emoji="<:zebra:911433583032422420>",
+                                        url=f"https://sharerepo.stkc.win/v2/?pkgman=zebra&repo={potential_repo}", style=discord.ButtonStyle.url))
+        view.add_item(discord.ui.Button(label='Other Package Managers', emoji=":cydiasileosplit:932650041099825232",
+                                        url=f"https://sharerepo.stkc.win/?repo={potential_repo}", style=discord.ButtonStyle.url))
+
+        await message.reply(file=discord.File("data/images/transparent1x1.png"), view=view, mention_author=False)
 
 
 def setup(bot):
